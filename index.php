07df6b72c4f54156d23632043cec4e32740f9a5a
S2Lyne <aaa_lyne@hotmail.com>
2021-01-18 22:21:23 -0300

    Atualizando repositorios

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+    <title>Formulário de Cadastro</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style type="text/css">
+    
+        
+    </style>
+</head>
+<body>
+    <div>
+    <nav class="navbar navbar-dark bg-primary" >
+        <a class="navbar-brand" style="margin-left: 40px;"><i class="fas fa-border-none"></i>ToDo</a>
+        <form class="form-inline">
+            <button class="btn btn-outline-secondary my-2 my-sm-0 mr-2" type="submit"><img src="../img/lupa.png" class="img-fluid"></button>
+            <input class="form-control mr-sm-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+            
+        </form>
+    </nav>
+        
+    </div>
+    <div class="container-fluid" style="margin-top: 40px">
+        <div class="row">
+            <div class="col-2">
+                <nav class="nav flex-column">
+                    <a class="nav-link active" href="#">Meu Dia</a>
+                    <a class="nav-link" href="#">Importante</a>
+                    <a class="nav-link" href="#">Planejado</a>
+                    <a class="nav-link" href="#">Atribuída a Você</a>
+                    <a class="nav-link" href="#">Email sinalizado</a>
+                    <a class="nav-link" href="#">Tasks</a>
+                </nav>
+            </div>
+        
+            <div class="col-4">
+                <form>
+                    <div class="form-group">
+                    <button><i class="fas fa-plus"></i></button>
+                        <label>Adicionar Tarefa</label>
+                    </div>
+                    <hr>   
+                    <div class="row">
+                    <label> </label>
+                        
+                    </div>
+                    </hr>   
+                    <div class="row">
+                   
+                </form>
+            </div>
+        </div>
+    </div>
+    
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
